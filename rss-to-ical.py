e0c859f26330c60a8a8c4a0b759a8b87727fd8ba
Joost Rijneveld <joost@joostrijneveld.nl>
2017-07-18 17:21:37 +0200

    Add initial version

---
+++ rss-to-ical.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import icalendar
+import feedparser
+import pytz
+import datetime
+import os
+
+app = Flask(__name__)
+
+# TODO make this dynamic
+
+if not os.environ['RSS_FEED_URL']:
+    sys.exit(1)
+
+@app.route("/")
+def get_feed():
+    feed = feedparser.parse(os.environ['RSS_FEED_URL'])
+    cal = icalendar.Calendar()
+
+    cal.add('prodid', '-//rss-to-ical-flask//')
+    cal.add('version', '2.0')
+
+    for post in feed.entries:
+        event = icalendar.Event()
+        event.add('dtstart', datetime.datetime(*post.published_parsed[:6],
+                                               tzinfo=pytz.timezone("GMT")))
+        event.add('dtend', datetime.datetime(*post.published_parsed[:6],
+                                             tzinfo=pytz.timezone("GMT")) +
+                           datetime.timedelta(hours=2))
+        event.add('dtstamp', datetime.datetime(*post.published_parsed[:6],
+                                               tzinfo=pytz.timezone("GMT")))
+        event.add('summary', post.title)
+        event.add('url', post.link)
+
+        cal.add_component(event)
+    return cal.to_ical()
